aa2cdfb74e174ab26bfeb81a4fc276fbbd661bcd
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-03-24 17:01:26 +0100

    Updated LBFGS

--- LBFGS/prova_bfgs.m
+++ LBFGS/prova_bfgs.m
@@ -2,27 +2,35 @@ clear;
 rng(1);
 
 % Build the X matrix from the dataset
-dataset = readtable('ML-CUP21-TR.csv');
+dataset = readtable('../datasets/ML-CUP21-TR.csv');
 dataset = table2array(dataset);
 X = dataset(:, 2:end);
 
 % Build \hat_{X} and \hat_{y}
 [m, n0] = size(X);
-X = [X'; eye(m)];
+lambda = 1e-5;
+X = [X'; lambda.*eye(m)];
 [m, n] = size(X);
 y = [randn(n0, 1); zeros(m-n0, 1)];
 
 w = randn(n, 1);
 
-XtX=(X'*X);
-ytX2= 2.*y'*X;
-yty=y'*y;
+% Build the function and its gradient
+XtX = X'*X;
+ytX2 = 2.*y'*X;
+yty = y'*y;
 
 grad_lls = @(x) 2.*x'*XtX - ytX2;
 f_lls = @(x) x'*XtX*x - ytX2*x + yty;
 
-l = 19;
+% Matlab output
+ris = X\y;
 
-%ris = X\y;
+% Compute LBFGS
+[ouptut, k] = LBFGS(w, f_lls, X, grad_lls, 14, 1e-5);
 
-[w, k] = LBFGS(w, f_lls, X, grad_lls, l, 1e-8);
+% Compute LFBGS for different configurations
+l = [3, 5, 10];
+tol = [1e-2, 1e-4, 1e-6];
+lambda = [1e-2, 1e-3, 1e-4, 1e-5];
+run_configurations(l, tol, lambda, f_lls, grad_lls, w, dataset);
