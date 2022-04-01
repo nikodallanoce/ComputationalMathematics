b010485a160dffa0e29c68c32a772dc29d3652a6
t3rmit4nk <official.simone.rizzo@gmail.com>
2022-04-01 20:20:15 +0200

    Armijowolf script added.

--- LBFGS/BLS.m
+++ LBFGS/BLS.m
@@ -1,7 +1,7 @@
-function a = BLS(f, grad, alpha, m1, tau, a, xk)
+function a = BLS(f, grad, alpha, m1, tau, a)
 al = alpha(0);
 phi_zero = f(al);
-grad_zero = grad(xk);
+grad_zero = grad(al);
 while(f(alpha(a))> phi_zero + m1*a*grad_zero)
     a=a*tau;
 end
