5b746f7db4cb2d66ec18d99ea1320acfb241ccef
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-04-01 21:35:54 +0200

    L-BFGS modified

--- LBFGS/LBFGS.m
+++ LBFGS/LBFGS.m
@@ -1,4 +1,4 @@
-function [x_next, k] = LBFGS(x0, f, X, grad, l, tol)
+function [x_next, k] = LBFGS(x0, f, X, grad, l, tol, Wolfe)
 %{
 LBFGS computes the solution to the linear least squares problem following
 algorithm 7.4 by Jorge Nocedal and Stephen J Wright. Numerical optimization.
@@ -9,7 +9,7 @@ Input:
     grad: (function) gradient of the function
     l: (int) number of steps to keep in memory
     tol: (float) tollerance on the error
-Ouptut:
+Output:
     x_next: (array) solution, or close to it, to the minimization problem
     k: (int) steps taken
 %}
@@ -24,8 +24,11 @@ norm_y= 9999;
 I = eye(size(X, 2));
 while(norm(grad_k)>tol && norm_y>tol)
     pk = -compute_direction(grad_k, s, y, I, k); % search direction
-    % alpha = ArmijoWolf(f, grad, @(alpha)xk + alpha.*pk, 1e-4, 1e-3, 0.5, 1, pk);
-    alpha = BLS(f, grad, @(alpha)xk + alpha.*pk, 1e-4, 0.5, 1);
+    if Wolfe
+        alpha = ArmijoWolfeLS(f, grad, @(alpha)xk + alpha.*pk, 1e-4, 0.9, 0.5, 1, pk);
+    else
+        alpha = BLS(f, grad, @(alpha)xk + alpha.*pk, 1e-4, 0.5, 1);
+    end
     x_next = xk + alpha.*pk;
     grad_next = grad(x_next)';
     yk = grad_next - grad_k;
@@ -41,4 +44,33 @@ while(norm(grad_k)>tol && norm_y>tol)
     % if the last gradient didn't have a great change from the previous one
     norm_y = norm(y(end));
 end
+end
+
+function [s, y] = memory_handling(s, y, xk, x_next, yk, k, l)
+%{
+Handles the memory available to the LBFGS method, where l is the size of
+the former.
+Input:
+    s: (matrix) l displacements defined as y_k=x_{k+1}-x_k, each displacement
+    is a column
+    y: (matrix) l differences between gradients defined as y_k=\nabla
+    xk: (array) current point
+    x_next: (array) new point
+    yk: (array) difference between gradient(x_next) and gradient(xk)
+    k: (int) current iteration
+    l: (int) number of steps to keep in memory
+Output:
+    s: (matrix) updated matrix of the displacements, where the most recent
+    one is in the last column
+    y: (matrix) updated matrix of the differences between gradients, where
+    the most recent one is in the last column
+%}
+
+    if k > l
+        s = [s(:, 2:end) x_next-xk];
+        y = [y(:, 2:end) yk];
+    else
+        s= [s x_next-xk];
+        y = [y yk];
+    end
 end
