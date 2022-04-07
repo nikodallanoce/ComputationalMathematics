9736b84f65674eb4d913fbf531a74eec39c63fb1
t3rmit4nk <official.simone.rizzo@gmail.com>
2022-04-07 17:40:18 +0200

    +Alpha_fun Issue with non termination of LBFGS

--- LBFGS/ArmijoWolf.m
+++ LBFGS/ArmijoWolf.m
@@ -6,16 +6,19 @@ a_start=1;
 sfgrd=0.01;
 tau=0.9;
 min_a=1e-16;
+    function [phi_a,phi_0,phi_p0,phi_ps] = alpha_fun(alphal)
+        alpha_f = @(alpha)xk + alpha.*pk; % alphafunction lambda alpha: xk+aplha*pk
+        phi_a = f(alpha_f(alphal));
+        last_g_x = f1(alpha_f(alphal));
+        phi_0 = f(xk);
+        phi_p0 = f1(xk);
+        phi_ps = last_g_x*pk;
+    end
 
 as = a_start;
 ls_iter = 1;
 while ls_iter < max_f_eval
-    alpha_f = @(alpha)xk + alpha.*pk; % alphafunction lambda alpha: xk+aplha*pk
-    phi_a = f(alpha_f(as));
-    last_g_x = f1(alpha_f(as));
-    phi_0 = f(xk);
-    phi_p0 = f1(xk);
-    phi_ps = last_g_x*pk;
+    [phi_a,phi_0,phi_p0,phi_ps] = alpha_fun(alphal);
     % Armijo and strong Wolf condition.
     if (all(phi_a <= phi_0 + m1 * as * phi_p0)) && all(abs(phi_ps) <= -m2*phi_p0)
         a = as;
@@ -36,12 +39,8 @@ while ls_iter <= max_f_eval && as - am > min_a && phi_ps > 1e-12
     a = (am * phi_ps - as * phi_pm) / (phi_ps - phi_pm);
     %a = max(am * (1 + self.sfgrd), min(_as * (1 - self.sfgrd), a))
     a = max(am + (as - am) * sfgrd, min(as - (as - am) * sfgrd, a));
-    alpha_f = @(alpha)xk + alpha.*pk; % alphafunction lambda alpha: xk+aplha*pk
-    phi_a = f(alpha_f(a));
-    last_g_x = f1(alpha_f(a));
-    phi_0 = f(xk);
-    phi_p0 = f1(xk);
-    phi_p = last_g_x*pk;
+
+    [phi_a,phi_0,phi_p0,phi_p] = alpha_fun(a);
     if all(phi_a <= phi_0 + m1 * a * phi_p0) && all(abs(phi_p) <= -m2 * phi_p0)
             break;
     end
