6e72ec724fd9e8d725c4417dac75365ea378b564
Niko Dalla Noce <dllnik97@gmail.com>
2022-04-11 16:14:39 +0200

    some tests

--- LBFGS/prova_bfgs.m
+++ LBFGS/prova_bfgs.m
@@ -1,5 +1,5 @@
 clear;
-% rng(1);
+%rng(1);
 
 % Build the X matrix from the dataset
 dataset = readtable("../datasets/ML-CUP21-TR.csv");
@@ -8,7 +8,7 @@ X = dataset(:, 2:end);
 
 % Build \hat_{X} and \hat_{y}
 [m, n0] = size(X);
-lambda = 1e-8;
+lambda = 1e-2;
 X = [X'; lambda.*eye(m)];
 [m, n] = size(X);
 y = [randn(n0, 1); zeros(m-n0, 1)];
@@ -28,13 +28,12 @@ ris = X\y;
 
 % Compute LBFGS
 
-[output, k] = LBFGS(w, f_lls, X, grad_lls, 14, 1e-8, true);
 [output_bls, k_bls] = LBFGS(w, f_lls, X, grad_lls, 14, 1e-8, false);
-disp(norm(output-output_bls));
-disp(norm(X*output-y));
-disp(norm(X*output_bls-y));
-disp(norm(X*ris-y));
+[output, k] = LBFGS(w, f_lls, X, grad_lls, 14, 1e-8, true);
 
+fprintf("norm_res_bls= %.8e | norm_res_wolfe=%.8e | norm_res_matl=%.8e\n \n", norm(X*output_bls-y), norm(X*output-y), norm(X*ris-y))
+fprintf("diff: %.8e\n \n", norm(output-output_bls));
+fprintf("rel_err -> BLS: %.15e | Wolfe: %.15e \n \n", norm(output_bls-ris)/norm(ris), norm(output-ris)/norm(ris))
 % Compute LFBGS for different configurations
 l = [3, 5, 10, 16];
 lambda = [1, 1e-2, 1e-4, 1e-9];
