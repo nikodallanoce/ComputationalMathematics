9ca8f1124a9fdaafc8bad6c0a8f9bcbb04ad1a95
Niko Dalla Noce <dllnik97@gmail.com>
2022-04-19 14:59:13 +0200

    Updated run configuration

--- LBFGS/prova_bfgs.m
+++ LBFGS/prova_bfgs.m
@@ -16,5 +16,18 @@ y = [randn(n0, 1); zeros(m-n0, 1)];
 
 % Compute LFBGS for different configurations
 l = [5, 10, 15, 20];
-lambda = [1, 1e-2, 1e-4, 1e-9];
-run_configurations_lbfgs(10, l, lambda, 1e-8, dataset, y, true);
+lambda = [1, 1e-2, 1e-4, 1e-8];
+runs = 3;
+[residues, errors, times, iters, config] = run_configurations_lbfgs(runs, l, lambda, 1e-8, dataset, y, true);
+
+[r, ~] = size(residues);
+stats = strings(r, 5);
+
+for i=1 : r
+    row = {config(i),...
+           sprintf("res: %e +- %e", mean(residues(i,:)), std(residues(i,:))),...
+           sprintf("err: %e +- %e", mean(errors(i,:)), std(errors(i,:))),...
+           sprintf("time: %e +- %e", mean(times(i,:)), std(times(i,:))),...
+           sprintf("it: %e +- %e", mean(iters(i,:)), std(iters(i,:)))};
+    stats(i,:) = row;
+end
