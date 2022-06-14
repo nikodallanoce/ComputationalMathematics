0b85eee492f931ea8a374e7c71291185d72f6023
Niko Dalla Noce <dllnik97@gmail.com>
2022-06-14 18:32:35 +0200

    run configuration updated

--- LBFGS/prova_cinese.m
+++ LBFGS/prova_cinese.m
@@ -9,7 +9,7 @@ X = dataset;
 
 % Build \hat{X} and \hat{y}
 [m, n0] = size(X);
-lambda = 1;
+lambda = 1e-1;
 X_hat = [X'; lambda.*eye(m)];
 [m, n] = size(X_hat);
 y = [randn(n0, 1); zeros(m-n0, 1)]; % This is actually \hat{y}
