1dffd3c4f455033c7213a35f26821c4c278f3bb9
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-06-16 19:17:18 +0200

    New methods inserted, still need to be implemented

---
+++ 3_CG/prova_cg.m
@@ -0,0 +1,8 @@
+clear;
+
+addpath ../utilities;
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-4);
+[f_lls, grad_lls] = build_lls(X_hat, y_hat);
+rmpath ../utilities;
+
+% Compute the solution using conjugate 
