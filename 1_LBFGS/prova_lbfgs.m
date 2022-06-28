ad817a49c4d88ca28e66412e994c5bd336873e0a
Niko Dalla Noce <dllnik97@gmail.com>
2022-06-28 18:11:42 +0200

    preconditioned CG

--- 1_LBFGS/prova_lbfgs.m
+++ 1_LBFGS/prova_lbfgs.m
@@ -1,23 +1,34 @@
 clear;
-rng(1);
+%rng(1);
 addpath ../utilities;
-[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-4);
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e0);
 [f_lls, grad_lls] = build_lls(X_hat, y_hat);
 rmpath ../utilities;
 
 % Compute the solution using L-BFGS
-[w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 10, 1e-12, true, true, w_star);
-
-p = zeros(1, k-1);
-for i=1:1:k-1
-    if p_errors(i+1) < 1e-14
-       p_errors(i+1) = 1e-14;
-    end
-    p(1, i) = log(p_errors(i+1))./log(p_errors(i));
-end
+[w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 20, 1e-10, true, true, w_star);
 
 %save('X_hat.mat', 'X_hat')
 %save('y_hat.mat', 'y_hat')
 %save('w.mat', 'w')
 
-semilogy(linspace(1, length(errors), length(errors)), errors)
+[m,n] = size(errors);
+quad = zeros(size(errors));
+lin = zeros(size(errors));
+lin(1) = errors(1,1);
+quad(1) = errors(1,1);
+for i = 2:n
+    lin(i) = lin(1)/(2^i);
+    if (i<10)
+        quad(i) = quad(1)/power(2, 2^i);
+    end
+end
+
+
+semilogy(linspace(1, length(errors), length(errors)), errors);
+hold on;
+grid on;
+semilogy(linspace(1, length(quad), length(quad)), quad);
+semilogy(linspace(1, length(lin), length(lin)), lin);
+legend(["BFGS", "quadratic", "linear"]);
+hold off;
