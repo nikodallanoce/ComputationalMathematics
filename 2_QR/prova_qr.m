d56ac41a3189ec96aa13ce1d7302adc23985fcb8
Niko Dalla Noce <dllnik97@gmail.com>
2022-07-06 00:54:52 +0200

    the fucking x0

--- 2_QR/prova_qr.m
+++ 2_QR/prova_qr.m
@@ -3,7 +3,7 @@ clear;
 %rng(1);
 
 addpath ../utilities;
-[X_hat, y_hat] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-4);
+[X_hat, y_hat] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e2);
 rmpath ../utilities;
 
 % Compute the thin QR factorization
@@ -15,3 +15,6 @@ x = linsolve(R1, Q1y);
 % Compute matlab solution
 [Q_t, R_t] = qr(X_hat, "econ");
 x_star = X_hat\y_hat;
+
+[Q1, R1] = thinqr(X_hat);
+upper = cond(X_hat)*norm(Q1'*(X_hat*x-y_hat))/norm(X_hat*x)
