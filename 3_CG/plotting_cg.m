345c4b976cf6f1c995843d4c9182078e84d28f8d
Niko Dalla Noce <dllnik97@gmail.com>
2022-07-07 01:03:59 +0200

    method for plotting cg varying lambda

---
+++ 3_CG/plotting_cg.m
@@ -0,0 +1,62 @@
+clear;
+
+addpath ../utilities;
+
+%lambdas = [1e5, 1e4, 1e3, 1e2, 1e1, 1e0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5];
+lambdas = [1e4, 1e2, 1e0, 1e-2, 1e-4];
+times = zeros(length(lambdas),1);
+errors = [];
+ks = zeros(length(lambdas),1);
+for i=1:length(lambdas)
+    [X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", lambdas(i));
+    %rmpath ../utilities;
+  
+    % Compute the solution using conjugate
+    
+    A = X_hat' * X_hat;
+    b = X_hat' * y_hat;
+
+    %fprintf("%e %e\n", lambdas(i), cond(A))
+    %disp([lambdas(i), cond(A)])
+
+    x0 = zeros(length(A),1);
+    
+    tol = 1e-12;
+    
+    time_elapsed = tic;
+    [x, k, err] = cg(A, x0, b, tol, w_star);
+    time_elapsed = toc(time_elapsed);
+    times(i) = time_elapsed;
+    errors = [errors err];
+    ks(i) = k;
+end
+
+linear = zeros(length(max(ks)), 1);
+linear(1) = errors(1,ks(3));
+for i = 1:max(ks)
+    linear(i) = linear(1)/(2^i);
+end
+s = 1;
+e = ks(1);
+semilogy(errors(s:e),'LineWidth',2);
+hold on;
+for i=2:length(ks)
+    s = e+1;
+    e = ks(i) + s - 1;
+    semilogy(errors(s:e), 'LineWidth',2);
+end
+semilogy(linear, 'LineWidth',2);
+hold off;
+grid on;
+
+x_lab = strings(length(lambdas)+1,1);
+for i=1:length(lambdas)
+    x_lab(i)=num2str(lambdas(i),"%.1e");
+end
+x_lab(end) = "linear";
+legend(x_lab);
+title("||w-w*|| by varying lambda values")
+xlabel("iterations");
+ylabel("Log10 error")
+
+%plot(times);
