66ec82284c305302188427dc6291e0de321b9bf8
t3rmit4nk <official.simone.rizzo@gmail.com>
2022-07-08 23:45:12 +0200

    Gradient descend with momentum. TODO find the problem with the direction

--- 4_SMD/gd.m
+++ 4_SMD/gd.m
@@ -1,14 +1,23 @@
-function [x, k] = gd(func, grad, x0, b, tol, eta, alpha)
+function [x, k] = gd(grad, x, tol, eta, alpha)
+%{
+Performs an Gradient descent approach with momentum.
+Inputs:
+    g       gradient of the function f
+    x       starting point
+    tol     tolerance for our norm(gradient)
+    eta     learning rate hyperparameter
+    aplha   momentum hyperparameter
+Output: solution of the problem and number of steps.
+%}
 
-k=0;
-x = x0;
-f=func(x);
+k = 0;
 df = grad(x)';
-old_delta_x = 0;
-
+v = 0;
 while(norm(df)>tol && k<100)
-
-    
+    v = v*alpha - eta*df;          % compute the direction v
+    x = x + v;                     % uptade x
+    df = grad(x)';                 % compute the new gradient
+    k = k + 1;
+    fprintf('%5d %1.2e\n', k, norm(df));
 end
-
 end
