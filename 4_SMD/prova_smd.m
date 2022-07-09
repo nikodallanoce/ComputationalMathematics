0e64b8ccbe798757e83368ba80d47c1f6e177b7d
t3rmit4nk <official.simone.rizzo@gmail.com>
2022-07-09 02:45:09 +0200

    Gradient descend now converge. TODO investigate about the role of ALPHA.

--- 4_SMD/prova_smd.m
+++ 4_SMD/prova_smd.m
@@ -1,7 +1,6 @@
 clear;
-
 addpath ../utilities;
-[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e2);
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e4);
 [f_lls, grad_lls] = build_lls(X_hat, y_hat);
 rmpath ../utilities;
 
@@ -10,5 +9,9 @@ b = X_hat' * y_hat;
 x0 = zeros(length(w_star),1);
 tol = 1e-12;
 
-[x, k] = gd(grad_lls, x0, tol, 0.005, 0.9);
+rows_number = size(X_hat);
+rows_number = rows_number(1);
+
+resid_fun = @(xk) norm(X_hat*xk-y_hat)/norm(y_hat);
+[x, k, errors, residuals] = gd(grad_lls, x0, w_star,resid_fun, tol, 0.00001, 0.2, rows_number);
 disp(norm(x-w_star));
