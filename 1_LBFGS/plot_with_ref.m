97be0109bf9f37f0bf69d7f9e6eff74656ee38fa
Niko Dalla Noce <dllnik97@gmail.com>
2022-07-09 13:02:33 +0200

    little changes including python script

--- 1_LBFGS/plot_with_ref.m
+++ 1_LBFGS/plot_with_ref.m
@@ -1,14 +1,13 @@
 
 clear;
-rng(1);
 addpath ../utilities;
 addpath ArmijoWolfeImplementations\;
-[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-4);
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e0);
 [f_lls, grad_lls] = build_lls(X_hat, y_hat);
 rmpath ../utilities;
 
 % Compute the solution using L-BFGS
-w = randn(size(w));
+w = zeros(size(w));
 [w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, sparse(X_hat), y_hat, 20, 1e-14, true, true, w_star);
 
 [m,n] = size(errors);
