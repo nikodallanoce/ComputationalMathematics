6b8104bb8172b1663fe2ab554c7fd1ce3d077894
t3rmit4nk <official.simone.rizzo@gmail.com>
2022-07-09 16:43:44 +0200

    new run configurations on LBFGS

--- 1_LBFGS/run_configurations_lbfgs.m
+++ 1_LBFGS/run_configurations_lbfgs.m
@@ -1,35 +1,23 @@
-function [residues, errors, times, iters, config] = run_configurations_lbfgs(runs, l, lambda, tol, D, y, Wolfe)
+function [residues, errors, times, iters, config] = run_configurations_lbfgs(runs, l, lambda, tol, Wolfe)
+addpath ../utilities;
 tot_runs = length(lambda)*length(l);
 residues = -ones(tot_runs, runs, 1000);
 errors = -ones(tot_runs, runs, 1000);
 times = zeros(tot_runs, runs);
 iters = zeros(tot_runs, runs);
 config = strings(tot_runs,1);
-[m0, ~] = size(D);
-% Compute LBFGS
-X = [D'; eye(m0)];
-[~, n] = size(X);
 for r=1: runs
-    fprintf("Run %d \n \n", r);
-    w = randn(n, 1);
+%     fprintf("Run %d \n \n", r);
     ind = 1;
     for i=1:length(l)
         curr_l=l(i);
         for j=1:length(lambda)
             curr_lambda = lambda(j);
-            X = [D'; curr_lambda.*eye(m0)];
-            [~, n] = size(X);
-            XtX = X'*X;
-            ytX2 = 2.*y'*X;
-            yty = y'*y;
-            grad_lls = @(x) 2.*x'*XtX - ytX2;
-            f_lls = @(x) x'*XtX*x - ytX2*x + yty;
-            x_star = X\y;
-            
+            [X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", lambda(j));
+            [f_lls, grad_lls] = build_lls(X_hat, y_hat);
+            w = zeros(size(w));            
             tic;
-            %[~, k, residue, error] = LBFGS(w, f_lls, X, grad_lls, curr_l, tol, Wolfe, y, x_star);
-
-            [~, k, residue, error] = LBFGS(w, f_lls, grad_lls, X, y, curr_l, tol, Wolfe, false, x_star);
+             [~, k, residue, error] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 5, 1e-14, true, false, w_star);
             elapsed = toc;
             fprintf("Config: l= %d, lambda= %.1e, resid= %e, error= %.6e, iter= %d, time= %.2f\n", curr_l, curr_lambda, residue(end), error(end), k, elapsed)
             residues(ind, r, 1:length(residue)) = residue;
