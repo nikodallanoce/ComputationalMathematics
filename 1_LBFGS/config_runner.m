ded4315064e0a3981aeabc3525eaad231407c7f8
t3rmit4nk <official.simone.rizzo@gmail.com>
2022-07-09 16:43:44 +0200

    new run configurations on LBFGS

--- 1_LBFGS/config_runner.m
+++ 1_LBFGS/config_runner.m
@@ -1,19 +1,7 @@
 clear;
 
-% Build the X matrix from the dataset
-dataset = readtable("../datasets/ML-CUP21-TR.csv");
-dataset = table2array(dataset);
-D = dataset(:, 2:end);
-
-% Build \hat{X} and \hat{y}
-[m, n0] = size(D);
-lambda = 1e-8;
-X_hat = [D'; lambda.*eye(m)];
-[m, n] = size(X_hat);
-y = [randn(n0, 1); zeros(m-n0, 1)]; % This is actually \hat{y}
-
 lambda = [1e4, 1e2, 1, 1e-2, 1e-4];
 l = [5, 10, 15, 20];
 
-[residues, errors, times, iters, config] = run_configurations_lbfgs(1, l, lambda, 1e-12, D, y, true);
+[residues, errors, times, iters, config] = run_configurations_lbfgs(3, l, lambda, 1e-12, true);
 %csvwrite('wolfe_errors_config_cin.csv',errors)
