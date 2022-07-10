83951c44d30e6a5fced3c515ccd744bc083eba72
Niko Dalla Noce <dllnik97@gmail.com>
2022-07-10 15:49:48 +0200

    cg opt saves also norm A

--- 3_CG/cg_opt.m
+++ 3_CG/cg_opt.m
@@ -1,14 +1,18 @@
-function [x_k, k, errors] = cg_opt(A, x_0, b, tol, x_star)
+function [x_k, k, errors, errors_A] = cg_opt(A, x_0, b, tol, w_star)
 r_k = A'*(A*x_0) - b; % residual
 p_k = -r_k; % search direction
 x_k = x_0; % current point
 k = 0; % iteration
 tolb = tol*norm(b); % stop condition
-errors = norm(x_star-x_k);
+norm_A = @(w) (w-w_star)'*A'*A*(w-w_star);
+errors = norm(w_star-x_k);
+errors_A = norm_A(x_0); 
 
-while(norm(r_k)>tolb)
+
+while(norm(r_k)>tolb && k<1000)
     [x_k, r_k, p_k] = iteration(A, r_k, p_k, x_k);
-    errors = [errors norm(x_star - x_k)];
+    errors = [errors norm(w_star - x_k)];
+    errors_A = [errors_A norm_A(x_k)];
     k = k+1;
 end
 end
--- 3_CG/run_configurations.m
+++ 3_CG/run_configurations.m
@@ -3,12 +3,13 @@ clear;
 addpath ../utilities;
 
 lambdas = [1e4, 1e2, 1e0, 1e-2, 1e-4];
-runs = 5;
-
-times = zeros(length(lambdas), runs);
-errors = cell(length(lambdas), runs);
-residuals = zeros(length(lambdas), runs);
-ks = zeros(length(lambdas), runs);
+runs = 10;
+max_iter = 30;
+times = -ones(runs, length(lambdas));
+errors = -ones(runs, length(lambdas), max_iter);
+errors_A = -ones(runs, length(lambdas), max_iter);
+residuals = -ones(runs, length(lambdas));
+ks = -ones(runs, length(lambdas));
 tol = 1e-14;
 
 for r=1:runs
@@ -19,12 +20,15 @@ for r=1:runs
         x0 = zeros(length(w_star), 1);
         X_hat = sparse(X_hat);
         time_start = tic;
-        [x, k, err] = cg_opt(X_hat, x0, b, tol, w_star);
-        times(i,r) = toc(time_start);
-        residuals(i,r) = norm(X_hat * x - y_hat)/norm(y_hat);
-        ks(i, r) = k;
+        [x, k, err, err_A] = cg_opt(X_hat, x0, b, tol, w_star);
+        times(r,i) = toc(time_start);
+        residuals(r,i) = norm(X_hat * x - y_hat)/norm(y_hat);
+        ks(r,i) = k;
         n2w = norm(w_star);
-        errors(i, r) = mat2cell(err/n2w, 1, length(err));
+        err = err(end);
+        err_A = [err_A(1),err_A(end)];
+        errors(r,i, 1:length(err)) = err/n2w;
+        errors_A(r,i, 1:length(err_A)) = err_A;
     end
     disp(r);
 end
