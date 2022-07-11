cd182bc5f59eeb7721c75f7ad3b12c55b9d4455d
Niko Dalla Noce <dllnik97@gmail.com>
2022-07-11 17:36:09 +0200

    bls takes too much time

--- 1_LBFGS/prova_lbfgs.m
+++ 1_LBFGS/prova_lbfgs.m
@@ -8,7 +8,7 @@ rmpath ../utilities;
 
 % Compute the solution using L-BFGS
 w = zeros(size(w));
-[w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 5, 1e-14, true, true, w_star);
+[w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 20, 1e-14, false, true, w_star);
 
 p = zeros(1, k-1);
 for i=1:1:k-1
