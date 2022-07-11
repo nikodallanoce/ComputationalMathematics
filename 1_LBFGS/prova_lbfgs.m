863eb62cee35f84dadde371010fea6a31a0d5c2a
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-07-11 11:06:13 +0200

    Small changes

--- 1_LBFGS/prova_lbfgs.m
+++ 1_LBFGS/prova_lbfgs.m
@@ -2,13 +2,13 @@ clear;
 rng(1);
 addpath ../utilities;
 addpath ArmijoWolfeImplementations\;
-[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-4);
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e4);
 [f_lls, grad_lls] = build_lls(X_hat, y_hat);
 rmpath ../utilities;
 
 % Compute the solution using L-BFGS
 w = zeros(size(w));
-[w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 5, 1e-14, true, true, w_star);
+[w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 20, 1e-14, true, true, w_star);
 
 p = zeros(1, k-1);
 for i=1:1:k-1
