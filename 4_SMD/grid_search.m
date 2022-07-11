6ca270b90c04cc8a8e1362da88ea2618972450e8
Niko Dalla Noce <dllnik97@gmail.com>
2022-07-11 17:36:09 +0200

    bls takes too much time

--- 4_SMD/grid_search.m
+++ 4_SMD/grid_search.m
@@ -1,6 +1,10 @@
 clear;
+
+lambdas = [1e4, 1e2, 1, 1e-2, 1e-4];
+etas = linspace(1e-5, 1e-2);
+
 addpath ../utilities;
-[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e2);
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-2);
 [f_lls, grad_lls] = build_lls(X_hat, y_hat);
 rmpath ../utilities;
 
@@ -12,5 +16,5 @@ tol = 1e-12;
 [rows_number, ~] = size(X_hat);
 
 resid_fun = @(xk) norm(X_hat*xk-y_hat)/norm(y_hat);
-[x, k, errors, residuals] = mgd(f_lls, grad_lls, x0, w_star, resid_fun, tol, 1e-3, 0.2, rows_number);
+[x, k, errors, residuals] = mgd(grad_lls, x0, w_star,resid_fun, tol, 1e-3, 0.2, rows_number);
 disp(norm(x-w_star));
--- 4_SMD/mgd.m
+++ 4_SMD/mgd.m
@@ -1,4 +1,4 @@
-function [x, k, errors, residuals] = gd(grad, x, x_star, resid_fun, tol, eta, alpha, n)
+function [x, k, errors, residuals] = mgd(f, grad, x, x_star, resid_fun, tol, eta, alpha, n)
 %{
 Performs an Gradient descent approach with momentum.
 Inputs:
@@ -13,19 +13,35 @@ Inputs:
               of the gradient.
 Output: solution of the problem, number of steps, errors and residuals.
 %}
-
+addpath("../1_LBFGS\ArmijoWolfeImplementations");
 k = 0;
-df = grad(x)';
 v = 0;
 errors = norm(x-x_star);
 residuals = resid_fun(x);
-while(norm(df)>tol && k<1e4)
+%eta = strong_wolfe_line_search(f, grad, -df, x);
+df = grad(x)';
+
+while(norm(df)>tol && k<2)
+    df = grad(x)';
+    eta = BLS(f, grad, x, 1e-4, 0.5, 1);
     v = v*alpha - eta*df./n;          % compute the direction v
     x = x + v;                        % uptade x
-    df = grad(x)';                    % compute the new gradient
+    %df = grad(x)'; % compute the new gradient
+    %eta = strong_wolfe_line_search(f, grad, v, x);
     k = k + 1;
     errors = [errors norm(x-x_star)];
     residuals = [residuals resid_fun(x)];
     fprintf('%5d %1.2e\n', k, norm(df));
 end
+end
+
+function alpha = BLS(f, grad, xk, c1, tau, alpha)
+f_xk = f(xk);
+grad_xk = grad(xk);
+iter = 0;
+while (f(xk - alpha * grad_xk) > f_xk - c1 * alpha * (grad_xk' * grad_xk))
+    alpha = alpha * tau;
+    iter = iter +1;
+end
+disp(iter);
 end
