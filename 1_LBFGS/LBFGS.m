31f6622eb3129992cfc679697347fbf3e3d4b1be
Niko Dalla Noce <dllnik97@gmail.com>
2022-07-17 20:03:37 +0200

    new version wolfe ls

--- 1_LBFGS/LBFGS.m
+++ 1_LBFGS/LBFGS.m
@@ -4,17 +4,18 @@ grad_k = grad(x0)';           % gradient at the current point
 s_mem = zeros(length(xk), l); % displacements between next and current points
 y_mem = zeros(length(xk), l); % displacements between next and current gradients
 x_next = zeros(length(xk));
-H0 = eye(size(X, 2));
+
 residuals = norm(X*xk-y)/norm(y);
 errors = norm(xk-x_star)/norm(x_star);
 p_errors = abs(f(xk)-f(x_star));
 for k=1:1:1000
-    pk = -compute_direction(grad_k, s_mem, y_mem, H0, k); % search direction
+    pk = -compute_direction(grad_k, s_mem, y_mem, k); % search direction
     % compute the step size by doing a line search
     if Wolfe
         %alpha = ArmijoWolfe(f, grad, pk, xk); %OLD implementation
-        alpha = strong_wolfe_line_search(f,grad,pk,xk); %JP implementation
+        %alpha = strong_wolfe_line_search(f,grad,pk,xk); %JP implementation
         %alpha = Strongwolfe(f,grad,pk,xk,f(xk),grad(xk)'); %CH implementation 
+        alpha = strong_wolfe(f, grad, xk, f(xk), grad_k, pk);
     else
         alpha = BLS(f, grad, xk, pk, 1e-4, 0.5, 1);
     end
@@ -61,7 +62,7 @@ for k=1:1:1000
         else
             norm_y = norm(y_mem(end));
         end
-        if norm_y < tol || norm(grad_k) < tol
+        if norm(grad_k) < tol || norm_y < tol
             break;
         end
     end
@@ -71,7 +72,7 @@ if verbose && mod(k, 5) ~= 0
 end
 end
 
-function r = compute_direction(gradient, s, y, H0, k)
+function r = compute_direction(gradient, s, y, k)
 %{
 Computes the search direction, p, for the current iteration of the LBFGS
 method.
@@ -100,16 +101,126 @@ for i = nc:-1:1
     q = q - alpha(i).* y(:, i);
 end
 
-gamma = 1;
+gamma = -1;
 if k > 1
-    gamma = s(:, nc)'*y(:, nc) / norm(y(:, nc))^2;
+    gamma = s(:, nc)'*y(:, nc) / (y(:, nc)'*y(:, nc));
 end
 
-H0 = gamma * H0;
-r = H0 * q;
+r = gamma * q;
 
 for i = 1:nc
     beta = rho(i) * y(:, i)' * r;
     r = r + s(:, i)*(alpha(i) - beta);
 end
+end
+
+function [alpha] = strong_wolfe(func, grad, x0, f0, g0, p)
+% function [alpha] = strong_wolfe(func,x0,f0,g0,p)
+% Compute a line search to satisfy the strong Wolfe conditions.
+% Algorithm 3.5. Page 60. "Numerical Optimization". Nocedal & Wright.
+% INPUTS:
+%  func: objective function handle.
+%  x0: [n,1] initial design vector.
+%  f0: initial function evaluation.
+%  g0: [n,1] initial objective gradient vector.
+%  p: [n,1] search direction vector.
+% OUTPUTS:
+% alpha: search length
+
+% initialize variables
+c1 = 1e-4;
+c2 = 0.9;
+alpha_max = 2.5;
+alpha_im1 = 0;
+alpha_i = 1;
+f_im1 = f0;
+dphi0 = transpose(g0)*p;
+i = 0;
+max_iters = 20;
+
+% search for alpha that satisfies strong-Wolfe conditions
+while true
+  
+  x = x0 + alpha_i*p;
+  f_i = func(x);
+  g_i = grad(x)';
+  if (f_i > f0 + c1*dphi0) || ( (i > 1) && (f_i >= f_im1) )
+    alpha = alpha_zoom(func, grad, x0,f0,g0,p,alpha_im1,alpha_i);
+    break;
+  end
+  dphi = transpose(g_i)*p;
+  if ( abs(dphi) <= -c2*dphi0 )
+    alpha = alpha_i;
+    break;
+  end
+  if ( dphi >= 0 )
+    alpha = alpha_zoom(func, grad, x0,f0,g0,p,alpha_i,alpha_im1);
+    break;
+  end
+  
+  % update
+  alpha_im1 = alpha_i;
+  f_im1 = f_i;
+  alpha_i = alpha_i + 0.8*(alpha_max-alpha_i);
+  
+  if (i > max_iters)
+    alpha = alpha_i;
+    break;
+  end
+  
+  i = i+1;
+  
+end
+
+end
+
+function [alpha] = alpha_zoom(func, grad, x0,f0,g0,p,alpha_lo,alpha_hi)
+% function [alpha] = alpha_zoom(func,x0,f0,g0,p,alpha_lo,alpha_hi)
+% Algorithm 3.6, Page 61. "Numerical Optimization". Nocedal & Wright.
+% INPUTS:
+%  func: objective function handle.
+%  x0: [n,1] initial design vector.
+%  f0: initial objective value.
+%  g0: [n,1] initial objective gradient vector.
+%  p: [n,1] search direction vector.
+%  alpha_lo: low water mark for alpha.
+%  alpha_hi: high water mark for alpha.
+% OUTPUTS:
+%  alpha: zoomed in alpha.
+
+% initialize variables
+c1 = 1e-4;
+c2 = 0.9;
+i = 0;
+max_iters = 20;
+dphi0 = transpose(g0)*p;
+
+while true
+  alpha_i = 0.5*(alpha_lo + alpha_hi);
+  alpha = alpha_i;
+  x = x0 + alpha_i*p;
+  f_i = func(x);
+  g_i = grad(x)';
+  x_lo = x0 + alpha_lo*p;
+  f_lo = func(x_lo);
+  if ( (f_i > f0 + c1*alpha_i*dphi0) || ( f_i >= f_lo) )
+    alpha_hi = alpha_i;
+  else
+    dphi = transpose(g_i)*p;
+    if ( ( abs(dphi) <= -c2*dphi0 ) )
+      alpha = alpha_i;
+      break;
+    end
+    if ( dphi * (alpha_hi-alpha_lo) >= 0 )
+      alpha_hi = alpha_lo;
+    end
+    alpha_lo = alpha_i;
+  end
+  i = i+1;
+  if (i > max_iters)
+    alpha = alpha_i;
+    break;
+  end
+end
+
 end
--- 1_LBFGS/prova_lbfgs.m
+++ 1_LBFGS/prova_lbfgs.m
@@ -1,15 +1,13 @@
 clear;
-
 addpath ../utilities;
 addpath ArmijoWolfeImplementations\;
-[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-0);
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-4);
 [f_lls, grad_lls] = build_lls(X_hat, y_hat);
 rmpath ../utilities;
-
 % Compute the solution using L-BFGS
 w = zeros(size(w));
-%[w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 10, 1e-14, true, true, w_star);
-[w_our, k, residuals, errors] = LBFGS_risto(w, f_lls, grad_lls, X_hat, y_hat, 10, 1e-14, true, w_star);
+[w_our, k, residuals, errors, p_errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 20, 1e-10, true, true, w_star);
+%[w_our, k, residuals, errors] = LBFGS(w, f_lls, grad_lls, X_hat, y_hat, 10, 1e-14, true, w_star);
 rmpath ArmijoWolfeImplementations\;
 
 %{
@@ -17,7 +15,7 @@ p = zeros(1, k-1);
 for i=1:1:k-1
     if p_errors(i+1) < 1e-14
        p_errors(i+1) = 1e-14;
-    end
+%     end
     p(1, i) = log(p_errors(i+1))./log(p_errors(i));
 end
 %}
@@ -28,7 +26,6 @@ end
 
 % semilogy(linspace(1, length(errors), length(errors)), errors)
 disp(norm(w_our-w_star)/norm(w_star));
-
 lin = zeros(size(errors));
 quad = zeros(size(errors));
 lin(1) = errors(1);
