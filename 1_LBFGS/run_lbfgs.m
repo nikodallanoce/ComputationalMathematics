4c55454518b8b07fa9b5aaced22321c1b6c6ceee
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-08-05 12:13:29 +0200

    Small changes

--- 1_LBFGS/run_lbfgs.m
+++ 1_LBFGS/run_lbfgs.m
@@ -1,6 +1,6 @@
 clear;
 addpath ../utilities;
-[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e4);
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e2);
 [f_lls, grad_lls] = build_lls(X_hat, y_hat);
 rmpath ../utilities;
 
