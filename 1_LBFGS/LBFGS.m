c0c250c2ebdcbea06e4ee38cde9f8b2301b9feb8
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-08-05 12:10:45 +0200

    Updated L-BFGS

--- 1_LBFGS/LBFGS.m
+++ 1_LBFGS/LBFGS.m
@@ -1,20 +1,19 @@
-function [xk, k, errors, residual] = LBFGS(x0, X, y, y_hat, l, tol, verbose, x_star)
+function [xk, k, x_hist] = LBFGS(x0, X, y, l, tol, verbose, max_iters)
 % Limited memory BFGS (L-BFGS)
 % Inputs:
 %       x0          starting point
 %       X           input matrix
 %       y           array of expected values of the form \hat{X}'*\hat{y}
-%       y_hat       starting array of expected values
 %       l           memory size
 %       tol         tolerance
 %       verbose     print state of the L-BFGS during the iterations
 %       x_star      optimal solution
+%       max_iters    maximum number of iterations
 %
 % Output:
 %       xk          solution
 %       k           number of iterations spent by the method
-%       errors      array of errors computed at each iteration
-%       residual    the final residual norm(\hat{X}w-\hat{y})/norm(\hat{y})
+%       x_hist      array that keeps track of all the points computed
 %
 % Reference:
 %       Algorithm 2 from our report, which is in turn based
@@ -26,20 +25,23 @@ function [xk, k, errors, residual] = LBFGS(x0, X, y, y_hat, l, tol, verbose, x_s
 xk = x0; % starting point
 grad = @(w) X'*(X*w) - y; % gradient
 grad_k = grad(x0);
-sm = []; % array of displacements
-ym = []; % array of differences between gradients
-residual = 0;
-% metrics
-errors = norm(xk-x_star)/norm(x_star);
+sm = zeros(length(xk), l); % matrix of displacements
+ym = zeros(length(xk), l); % matrix of differences between gradients
+
+% keep track of all the points computed by the method
+x_hist = zeros(length(xk), max_iters+1);
+x_hist(:, 1) = xk;
 
 % print starting state of L-BFGS
 if verbose
-    fprintf('%5d %1.2e %1.2e\n', 0, errors(end), norm(grad_k));
+    fprintf("\tk norm(grad)\n");
+    fprintf('%5d %1.2e\n', 0, norm(grad_k));
 end
 
-for k=1:1000
+nc = 1; % number of filled columns of sm and ym
+for k=1:max_iters
     % compute search direction
-    pk = -compute_direction(grad_k, sm, ym, k);
+    pk = -compute_direction(grad_k, sm, ym, nc);
 
     % compute step size by exact line search
     A_pk = X*pk;
@@ -52,46 +54,50 @@ for k=1:1000
     grad_k = grad(xk);
 
     % memory handling
-    sm = [sm xk - xk_prev];
-    ym = [ym grad_k - grad_prev];
-
-    if k>l
-        sm(:, 1) = [];
-        ym(:, 1) = [];
+    if k > l
+        sm(:, 1:end-1) = sm(:, 2:end);
+        ym(:, 1:end-1) = ym(:, 2:end);
+        sm(:, end) = xk - xk_prev;
+        ym(:, end) = grad_k - grad_prev;
+        nc = l;
+    else
+        sm(:, k) = xk - xk_prev;
+        ym(:, k) = grad_k - grad_prev;
+        nc = k;
     end
 
-    if (sm(:, end)'*ym(:, end)<=0)
+    if (sm(:, nc)' * ym(:, nc) <= 0)
         fprintf("Curvature condition does not hold\n");
     end
 
     % compute metrics
-    errors = [errors norm(xk-x_star)/norm(x_star)];
+    x_hist(:, k+1) = xk;
 
     % print current state of L-BFGS
     if verbose && (mod(k, 5) == 0 || k == 1)
-         fprintf('%5d %1.2e %1.2e\n', k, errors(end), norm(grad_k));
+         fprintf('%5d %1.2e\n', k, norm(grad_k));
     end
 
     % stop if the norm of the direction is less than the tolerance
     if norm(pk) <= tol
-        residual = norm(X*xk-y_hat)/norm(y_hat);
         break;
     end
 end
 
 % print final state of L-BFGS
 if verbose && mod(k, 5) ~= 0
-     fprintf('%5d %1.2e %1.2e\n', k, errors(end), norm(grad_k));
+     fprintf('%5d %1.2e\n', k, norm(grad_k));
 end
+x_hist = x_hist(:, 1:k+1);
 end
 
-function r = compute_direction(grad, s, y, k)
+function r = compute_direction(grad, s, y, nc)
 % Two loop recursion needed to compute the search direction for L-BFGS
 % Inputs:
 %       grad        gradient computed at current point
 %       s           displacements
 %       y           differences between gradients
-%       k           current iteration
+%       nc          number of filled columns of s and y
 %
 % Output:
 %       r           H_k*\nabla f
@@ -103,21 +109,21 @@ function r = compute_direction(grad, s, y, k)
 %
 %Created by Niko Dalla Noce, Alessandro Ristori and Simone Rizzo
 
-if k == 1
+if nc == 1
     r = grad;
 else
     q = grad;
-    for i = size(s, 2):-1:1
+    for i = nc:-1:1
         rho(i) = 1 / (y(:, i)' * s(:, i));
         alpha(i) = rho(i).* s(:, i)' * q;
         q = q - alpha(i).* y(:, i);
     end
     
     % we do not need to explicitly use H_k^0
-    gamma = s(:, end)'* y(:, end) / norm(y(:, end))^2;
+    gamma = s(:, nc)'* y(:, nc) / norm(y(:, nc))^2;
     r = gamma.* q;
     
-    for i = 1:size(s, 2)
+    for i = 1:nc
         beta = rho(i) * y(:, i)' * r;
         r = r + s(:, i) * (alpha(i) - beta);
     end
