e7dc25dfd3acfb2c3058e8555a6d78fa0882da64
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-08-05 12:10:45 +0200

    Updated L-BFGS

--- 1_LBFGS/run_lbfgs.m
+++ 1_LBFGS/run_lbfgs.m
@@ -1,27 +1,14 @@
 clear;
 addpath ../utilities;
-[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e-4);
+[X_hat, y_hat, w, w_star] = build_matrices("../datasets/ML-CUP21-TR.csv", 1e4);
 [f_lls, grad_lls] = build_lls(X_hat, y_hat);
 rmpath ../utilities;
 
 % Compute the solution using L-BFGS
-w = zeros(size(w));
-b = X_hat'*y_hat;
-[w_our, k, errors, resid] = LBFGS(w, sparse(X_hat), b, y_hat, 20, 1e-14, true, w_star);
+w = zeros(size(w)); % starting point
+b = X_hat' * y_hat;
+[w_our, k, w_hist] = LBFGS(w, sparse(X_hat), b, 20, 1e-14, true, 1000);
 
-disp(norm(w_our-w_star)/norm(w_star));
-lin = zeros(size(errors));
-quad = zeros(size(errors));
-lin(1) = errors(1);
-quad(1) = errors(1);
-for i = 2:length(errors)
-    lin(i) = lin(1)/power(2, i);
-    if(i<10)
-        quad(i) = quad(1)/power(2, power(2, i));
-    end
-end
-semilogy(lin);
-hold on
-semilogy(quad);
-semilogy(errors)
-hold off
+% Compute the relative errors and the final residual
+rel_errors = vecnorm(w_hist - w_star) / norm(w_star);
+residual = norm(X_hat * w_hist(:, end) - y_hat) / norm(y_hat);
