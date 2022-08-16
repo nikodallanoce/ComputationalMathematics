c65ee25f1f5d71aea016ec6a1790f86facc2d872
Niko Dalla Noce <dllnik97@gmail.com>
2022-08-16 12:00:56 +0200

    SMD refactoring

--- 4_SMD/run_smd.m
+++ 4_SMD/run_smd.m
@@ -11,24 +11,9 @@ b = X_hat' * y_hat;
 x0 = zeros(length(w_star),1);
 tol = 1e-12;
 
-[rows_number, ~] = size(X_hat);
-
 grad = @(x) X_hat'*(X_hat*x)- b;
 
-[x, k, error_abs] = mgd_eqn(X_hat, grad, x0, w_star, tol, 0.03, 1e4, false, true);
+[x, k, w_hist] = mgd_eqn(X_hat, grad, x0, tol, 0.03, 1e4, true);
 disp(norm(x-w_star)/norm(w_star));
-
-errors = error_abs./norm(w_star);
-lin = zeros(size(errors));
-sub = zeros(size(errors));
-lin(1) = errors(1);
-sub(1) = errors(1);
-for i = 2:length(errors)
-    lin(i) = lin(1)/power(2,i);
-    sub(i) = sub(1)/i;
-end
-
-semilogy(sub);
-hold on;
-semilogy(errors)
-hold off
+re_errors = vecnorm(w_hist - w_star) / norm(w_star);
+residual = norm(X_hat * w_hist(:, end) - y_hat) / norm(y_hat);
