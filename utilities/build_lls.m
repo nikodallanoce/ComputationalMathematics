8d995ee82e6a5e1a885529ff1a980259badc8455
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-08-18 11:43:12 +0200

    Documented more methods

--- utilities/build_lls.m
+++ utilities/build_lls.m
@@ -1,4 +1,18 @@
 function [f_lls, grad_lls] = build_lls(X_hat, y_hat)
+% Build the matlab function that will be used by the methods
+% Inputs:
+%       X_hat       input matrix
+%       y_hat       array of expected values
+%
+% Output:
+%       f_lls       least mean squares function, (1.2) from our report
+%       grad_lls    least mean squares gradient, (1.3) from our report
+%
+% Reference:
+%       Equations (1.2) and (1.3) from our report.
+%
+% Created by Niko Dalla Noce, Alessandro Ristori and Simone Rizzo
+
 % Build the LLS function and gradient
 X_hat = sparse(X_hat);
 y_hat = sparse(y_hat);
